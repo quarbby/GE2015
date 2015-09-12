1b9832a78497197c3e43cb8c2450db83d191d549
Lynnette Ng <lynnette-ng@outlook.com>
2015-09-12 08:42:04 +0000

    Added some quotes

--- quotes.js
+++ quotes.js
@@ -1,15 +1,36 @@
 var quotes = '[ \
-        {"text" : "Singaporeans got the government they deserve. I don\'t want to hear any more complains.", \
-        "cite" : "K. Jeyaretnam, Reform Party", \
-        "video": "https://www.facebook.com/ChannelNewsAsiaSingapore/videos/vb.93889432933/10153136340652934/"}, \
+        {"text" : "Reporter confuses Teo Ser Luck and Teo Chee Hean ", \
+        "cite" : "CNA News Anchor", \
+        "video": "https://www.facebook.com/ChannelNewsAsiaSingapore/videos/10153136246587934/"}, \
         {"text" : "Wait First Lah", \
         "cite" : "Low Thia Khiang, Workers\' Party", \
         "video": "https://www.facebook.com/ChannelNewsAsiaSingapore/videos/10153136707622934/"}, \
+       {"text" : "Singaporeans got the government they deserve. I don\'t want to hear any more complains.", \
+        "cite": "K. Jeyaretnam, Reform Party", \
+        "video": "https://www.facebook.com/ChannelNewsAsiaSingapore/videos/vb.93889432933/10153136340652934/" }, \
+        {"text": "Singapore Flash, an ardent PAP supporter", \
+        "cite": "Mothership.sg", \
+        "pic" : "http://cdn.mothership.sg/wp-content/uploads/2015/09/0480af053bda423ab9382e01ba60c7c5.jpg" }, \
         {"text" : "We wanted to present to Singaporeans a message of inspiration, we wanted to present to them an alternative vision for Singapore", \
         "cite" : "Chee Soon Juan, SDP", \
-        "video": "https://www.facebook.com/ChannelNewsAsiaSingapore/videos/10153136676367934/"}, \
+        "video" : "https://www.facebook.com/ChannelNewsAsiaSingapore/videos/10153136676367934/" },\
+        {"text" : "Only 2.5m can vote - can I safely say 3m are not true blue Singaporeans?", \
+        "cite" : "Eugene Yeo, NSP", \
+        "pic" : "https://scontent-sin1-1.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/11990393_866805150036037_8741571549851957406_n.jpg?oh=dcb8d716c8c60643cc6dd0168a7a1fac&oe=56A77533"}, \
         {"text" : "Return Our CPF!", \
         "cite" : "Han Hui Hui, Independent", \
-        "pic": "https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-xtl1/t31.0-8/11953367_10153136596092934_8686002513386972262_o.jpg"} \
+        "pic": "https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-xtl1/t31.0-8/11953367_10153136596092934_8686002513386972262_o.jpg"}, \
+        {"text" : "Vote For PA.. I mean, Reform Party", \
+        "cite" : "M Ravi, Reform Party", \
+        "video" : "https://www.facebook.com/mrbrownlah/videos/863885086994710"}, \
+        {"text" : "Vote for Worker Party...", \
+        "cite" : "Jesse Loo, Reform Party", \
+        "video" : "https://www.facebook.com/TheStraitsTimes/videos/10152994680122115"}, \
+        {"text" : "So I am thinking, if we didn\’t separate in 1965, today you and I would be Malaysians, \‘heng\’ ah.", \
+        "cite" : "Lim Swee Say, PAP", \
+        "video": "https://www.facebook.com/ch8news/videos/1075049149180067"}, \
+        {"text" : "SingFirst and their Spell Check", \
+        "cite" : "Sing First", \
+        "pic": "https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-xtf1/v/t1.0-9/s720x720/11813280_674064246063774_5907232010038612576_n.jpg?oh=a725624fed6ae8a373cc504c4a6b243c&oe=5661704A&__gda__=1454004580_8fe3c1312ae2242c2cb878eebe179223"} \
 ]';
 
